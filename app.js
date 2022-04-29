f2a206d8e752b025d87809b9adcd8fbdd3c5ec9b
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-29 14:01:53 +0500

    declared required varibles

---
+++ app.js
@@ -0,0 +1,8 @@
+let dice_1_img = document.createElement('img');
+let dice_2_img = document.createElement('img');
+
+let message = document.getElementById('message');
+let points = document.getElementById('points')
+
+let dices = [1, 2, 3, 4, 5, 6];
+let rollCount = 0;
